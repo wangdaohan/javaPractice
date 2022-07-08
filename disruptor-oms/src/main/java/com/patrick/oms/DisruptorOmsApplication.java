81e618625db253aed290b35f4eeeb36bbd4dec4e
patricklo <patrick.jh.lo@gmail.com>
2022-07-08 22:30:35 +0800

    Spring Cloud+Vertx+Disruptor 撮合交易系统实战

---
+++ disruptor-oms/src/main/java/com/patrick/oms/DisruptorOmsApplication.java
@@ -0,0 +1,13 @@
+package com.patrick.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisruptorOmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisruptorOmsApplication.class, args);
+	}
+
+}
---
+++ disruptor-oms/src/test/java/com/patrick/oms/DisruptorOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisruptorOmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
